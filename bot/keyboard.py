71cce7824eb0dce47bc98944892b43f84da485c4
DaryaKhatsuk <DaryaKhatsuk>
2022-12-05 20:32:59 +0300

    commit

--- bot/keyboard.py
+++ bot/keyboard.py
@@ -5,10 +5,14 @@ from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButt
 start = types.ReplyKeyboardMarkup(resize_keyboard=True)
 info = types.KeyboardButton('Inform')
 stats = types.KeyboardButton('Statistic')
-motorcycles = types.KeyboardButton('Motorcycles')
+pols = types.KeyboardButton('Покажи пользователя')
 
-start.add(stats, info, motorcycles)
+start.add(stats, info, pols)
 
 stats_keyb = InlineKeyboardMarkup()
 stats_keyb.add(InlineKeyboardButton(f'yes', callback_data='join'))
 stats_keyb.add(InlineKeyboardButton(f'no', callback_data='cancel'))
+
+pols_keyb = InlineKeyboardMarkup()
+pols_keyb.add(InlineKeyboardButton(f'хочу увидеть id', callback_data='pols_key'))
+pols_keyb.add(InlineKeyboardButton(f'вернутся обратно', callback_data='back_start'))
--- bot/main.py
+++ bot/main.py
@@ -3,6 +3,7 @@ import asyncio
 import logging
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, \
     InlineKeyboardButton
+from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher import Dispatcher
 from aiogram.dispatcher.filters import Command
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -27,6 +28,60 @@ class Me_info(StatesGroup):
     Q2 = State()  # задаем состояние 2
 
 
+@dp.message_handler(Command('me'), storage=None)
+async def enter_me_info(message: types.Message):
+    if message.chat.id == config.admin:
+        await message.answer("Начинаем настройку \n"
+                             "1. Укажите ссылку на Ваш профиль")
+        await Me_info.Q1.set()
+
+
+@dp.message_handler(state=Me_info.Q1)
+# нашей функции мы указываем, что хотим получить сообщение и состояние
+async def answer_for_state_Q1(message: types.Message, state: FSMContext):
+    # сохраняем текст полученного сообщения
+    answer = message.text
+    # в данном месте прописываем для нашего состояния обновление данных
+    # в пространство имен для текущего состояния
+    # мы добавляем ключ answer1 со значением answer, далее мы в этом убедимся
+    await state.update_data(answer1=answer)
+    # после чего выводим сообщение
+    await message.answer("Ваша ссылка сохранена \n"
+                         "2. Введите текст")
+    # и задаем состояние Q2
+    await Me_info.Q2.set()
+
+
+@dp.message_handler(state=Me_info.Q2)
+async def answer_for_state_Q2(message: types.Message, state: FSMContext):
+    # записываем ответ
+    answer = message.text
+    # Снова в пространство имен добавляем answer2 со значением answer, т.е. с текстом пользователя
+    await state.update_data(answer2=answer)
+    # говорим боту отправить сообщение
+    await message.answer("Текст сохранен")
+    # в переменную data получаем словарь, хранящийся в нашем хранилище состояний для текущего состояния
+    data = await state.get_data()
+    # print(data)
+    # достаем значение по ключу answer1
+    answer1 = data.get("answer1")
+    # достаем значение по ключу answer2
+    answer2 = data.get("answer2")
+    # открываем файл link.txt на режим записи в кодировке UTF-8
+    with open("link.txt", 'w', encoding="UTF-8") as link_txt:
+        # записываем строкой ссылку в наш файл
+        link_txt.write(str(answer1))
+    # открываем файл text.txt в режиме записи в той же кодировке
+    with open("text.txt", "w", encoding="UTF-8") as text_txt:
+        # записываем в файл текст, который передал пользователь
+        text_txt.write(str(answer2))
+    # говорим боту отправить сообщение
+    await message.answer(f"Ваша ссылка на профиль: {answer1} \n"
+                         f"Ваш текст: {answer2}")
+    # закрываем текущее состояние
+    await state.finish()
+
+
 @dp.message_handler(Command('start'), state=None)
 async def welcome(message):
     joinedFile = open('user.txt', 'r')
@@ -41,6 +96,40 @@ async def welcome(message):
 
     await bot.send_message(message.chat.id, f'Hello, *{message.from_user.first_name},* bot worked',
                            reply_markup=keyboard.start, parse_mode='Markdown')
+
+
+@dp.callback_query_handler(text_contains='pols_key')
+async def pols_key(call: types.CallbackQuery):
+    await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                text=f'Ваш id: {call.message.chat.id}', parse_mode='Markdown')
+
+
+@dp.callback_query_handler(text_contains='back_start')
+async def back_start(call: types.CallbackQuery):
+    await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                text="Вы вернулись в главное меню")
+
+
+@dp.callback_query_handler(text_contains='join')
+async def join(call: types.CallbackQuery):
+    if call.message.chat.id == config.admin:
+        d = sum(1 for line in open('user.txt'))
+        await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                    text=f'Statistic: {d}', parse_mode='Markdown')
+    else:
+        await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                    text=f'You are not an administrator.')
+
+
+@dp.callback_query_handler(text_contains='cancel')
+async def cancel(call: types.CallbackQuery):
+    await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                text="Вы вернулись в главное меню")
+
+
+@dp.message_handler(commands='info')
+async def cmd_test1(message: types.Message):
+    await message.reply("I'm evil")
 
 
 @dp.message_handler(commands='rassilka')
@@ -68,11 +157,6 @@ async def rassilka(message):
                                                  f'заблокировали бота: *{block_users}*', parse_mode='Markdown')
 
 
-@dp.message_handler(commands='info')
-async def cmd_test1(message: types.Message):
-    await message.reply("I'm evil")
-
-
 @dp.message_handler(content_types='text')
 async def get_message(message):
     if message.text == 'Inform':
@@ -81,23 +165,6 @@ async def get_message(message):
     if message.text == 'Statistic':
         await bot.send_message(message.chat.id, text=f'{message.chat.first_name}, do you want see static?',
                                reply_markup=keyboard.stats_keyb, parse_mode='Markdown')
-
-
-@dp.callback_query_handler(text_contains='join')
-async def join(call: types.CallbackQuery):
-    if call.message.chat.id == config.admin:
-        d = sum(1 for line in open('user.txt'))
-        await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                    text=f'Statistic: {d}', parse_mode='Markdown')
-    else:
-        await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                    text=f'You are not an administrator.')
-
-
-@dp.callback_query_handler(text_contains='cancel')
-async def cancel(call: types.CallbackQuery):
-    await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                text="Вы вернулись в главное меню")
 
 
 if __name__ == "__main__":
